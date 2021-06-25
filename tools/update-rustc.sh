630de37f533497a297021d0f40992bfd2cdef19f
Ralf Zerres <ralf.zerres@networkx.de>
2021-06-25 20:04:54 +0200

    update to rust 1.53
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- tools/update-rustc.sh
+++ tools/update-rustc.sh
@@ -14,9 +14,9 @@ find -s listings -name Cargo.toml -print0 | while IFS= read -r -d '' f; do
     # There are a handful of listings we don't want to rustfmt and skipping doesn't work;
     # those will have a file in their directory that explains why.
     if [ ! -f "${dir_to_fmt}/rustfmt-ignore" ]; then
-        cd $dir_to_fmt
-        cargo fmt --all && true
-        cd - > /dev/null
+	cd $dir_to_fmt
+	cargo fmt --all && true
+	cd - > /dev/null
     fi
 done
 
@@ -36,8 +36,13 @@ find -s listings -name output.txt -print0 | while IFS= read -r -d '' f; do
 
     cd $tmp_build_directory
 
-    # Save the previous compile time; we're going to keep it to minimize diff churn
+    # Save the previous compile time; we're going to keep it to minimize diff
+    # churn
     compile_time=$(sed -E -ne "s/.*Finished (dev|test) \[unoptimized \+ debuginfo] target\(s\) in ([0-9.]*).*/\2/p" ${full_output_path})
+
+    # Save the hash from the first test binary; we're going to keep it to
+    # minimize diff churn
+    test_binary_hash=$(sed -E -ne 's@.*Running [^[:space:]]+ \(target/debug/deps/[^-]*-([^\s]*)\)@\1@p' ${full_output_path} | head -n 1)
 
     # Act like this is the first time this listing has been built
     cargo clean
@@ -59,7 +64,15 @@ find -s listings -name output.txt -print0 | while IFS= read -r -d '' f; do
 
     # Restore the previous compile time, if there is one
     if [ -n  "${compile_time}" ]; then
-        sed -i '' -E -e "s/Finished (dev|test) \[unoptimized \+ debuginfo] target\(s\) in [0-9.]*/Finished \1 [unoptimized + debuginfo] target(s) in ${compile_time}/" ${full_output_path}
+	sed -i '' -E -e "s/Finished (dev|test) \[unoptimized \+ debuginfo] target\(s\) in [0-9.]*/Finished \1 [unoptimized + debuginfo] target(s) in ${compile_time}/" ${full_output_path}
+    fi
+
+    # Restore the previous test binary hash, if there is one
+    if [ -n "${test_binary_hash}" ]; then
+	replacement='s@Running ([^[:space:]]+) \(target/debug/deps/([^-]*)-([^\s]*)\)@Running \1 (target/debug/deps/\2-'
+	replacement+="${test_binary_hash}"
+	replacement+=')@g'
+	sed -i '' -E -e "${replacement}" ${full_output_path}
     fi
 
     cd - > /dev/null
