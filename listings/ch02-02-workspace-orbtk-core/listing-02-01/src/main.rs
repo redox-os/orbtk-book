84be2850ffa1f89d5373488faae4fe4a1eb67d51
Ralf Zerres <ralf.zerres@networkx.de>
2021-07-01 17:21:24 +0200

    ch02-02-workspace-orbtk-core: update
    * update orbtk-core submodule text
    * add localization example
    * add app image
    * update source code
    
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- listings/ch02-02-workspace-orbtk-core/listing-02-01/src/main.rs
+++ listings/ch02-02-workspace-orbtk-core/listing-02-01/src/main.rs
@@ -1,19 +1,25 @@
 use orbtk::prelude::*;
 
+/*
+ * if you prefere to see a german translation
+ * static LOCALIZATION_DE_DE: &str = include_str!("../assets/localization/dictionary_de_DE.ron");
+ */
+
 // ANCHOR: Localization
-static LOCALIZATION_DE_DE: &str = include_str!("../assets/localization/dictionary_de_DE.ron");
 static LOCALIZATION_ES_ES: &str = include_str!("../assets/localization/dictionary_es_ES.ron");
 // ANCHOR_END: Localization
 
 fn main() {
     // if no dictionary is set for the default language e.g. english
     // the content of the text property will be drawn.
+    /* disabled german translation file
+     * let _de_de = RonLocalization::create()
+     *    .language("de_DE")
+     *    .dictionary("de_DE", LOCALIZATION_DE_DE)
+     *    .build();
+     */
     // ANCHOR: Language
-    let _de_de = RonLocalization::create()
-        .language("de_DE")
-        .dictionary("de_DE", LOCALIZATION_DE_DE)
-        .build();
-    let _es_es = RonLocalization::create()
+    let es_es = RonLocalization::create()
         .language("es_ES")
         .dictionary("es_ES", LOCALIZATION_ES_ES)
         .build();
@@ -24,28 +30,30 @@ fn main() {
 
     // ANCHOR: Application
     Application::new()
-        .localization(_es_es)
+        .localization(es_es)
         // ANCHOR_END: Application
         .window(|ctx| {
             Window::new()
-                .title("OrbTk - Example Localization")
+                .title("OrbTk-Book - Chapter 2.2")
                 .position((100.0, 100.0))
-                .size(420.0, 240.0)
+                .size(450.0, 140.0)
                 .child(
                     Stack::new()
                         .spacing(8)
                         .v_align("center")
                         .child(
                             TextBlock::new()
-                                .text("Hey OrbTK User!")
+                                .font_size(28)
                                 .h_align("center")
+                                .text("Hey OrbTK User!")
                                 .v_align("center")
                                 .build(ctx)
                         )
                         .child(
                             TextBlock::new()
-                                .text("Do you like it?")
+                                .font_size(28)
                                 .h_align("center")
+                                .text("Do you like it?")
                                 .v_align("center")
                                 .build(ctx)
                         )
