af48859b392d31d96c55d1804944412d2e55412e
Ralf Zerres <ralf.zerres@networkx.de>
2022-05-06 18:09:41 +0200

    ci/validate.sh: introduce argument `language`
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- ci/validate.sh
+++ ci/validate.sh
@@ -1,4 +1,6 @@
-for file in src/*.md ; do
+language=${1}
+
+for file in src/"$language"/*.md ; do
     echo Checking references in $file
     cargo run --quiet --bin link2print < $file > /dev/null
 done
